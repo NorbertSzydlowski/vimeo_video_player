bccd9eb753d12bc176765408fb4139927b4163b6
Norbert <zxcv1991_39@o2.pl>
2025-04-02 10:46:55 +0200

    Update vimeo_player.dart

--- lib/src/vimeo_player.dart
+++ lib/src/vimeo_player.dart
@@ -1,7 +1,7 @@
 import 'dart:developer' as dev;
 
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:talkjs_flutter_inappwebview/talkjs_flutter_inappwebview.dart';
 
 /// Vimeo video player with customizable controls and event callbacks using the InAppWebView
 class VimeoVideoPlayer extends StatelessWidget {
